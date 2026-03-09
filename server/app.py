9b2bbbf5dc752b406b7bddb564f0807b160e9fcb
Bildad Ochieng Masaga <bijasoto@gmail.com>
2026-03-10 00:03:57 +0300

    added base route and its variables

--- server/app.py
+++ server/app.py
@@ -2,3 +2,17 @@ from flask import Flask
 app = Flask(__name__)
 
 existing_models = ['Beedle', 'Crossroads', 'M2', 'Panique']
+
+@app.route('/')
+def index():
+    return '<h1>Welcome to Flatiron Cars<h1>'
+
+@app.route('/<model>')
+def car_model(model):
+    if model in existing_models:
+        return f'Flatiron {model} is in our fleet!'
+    else:
+        return f'No models called {model} exists in our catalog'
+    
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
